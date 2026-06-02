c8089de26be39c66c37f84b39bcc552c5bcc602d
mariowakim38-blip <mariowakim38@gmail.com>
2026-06-02 16:18:32 +0300

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.ihdeeiqwzjazuuwxkusz!
+const supabaseUrl = process.env.https://ihdeeiqwzjazuuwxkusz.supabase.co!
 const supabaseAnonKey = process.env.sb_publishable_wUO7RLFQ7_MFbxKb26U5ug_AiVELXO_!
 
 export const supabase = createClient(
