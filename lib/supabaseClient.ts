05c856b4e6e8cfc8daa08735a491252d0a2738f0
mariowakim38-blip <mariowakim38@gmail.com>
2026-06-02 16:04:38 +0300

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,3 +1,6 @@
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClientComponentClient()
+const supabaseUrl = process.env.https://ihdeeiqwzjazuuwxkusz.supabase.co!
+const supabaseAnonKey = process.env.sb_publishable_wUO7RLFQ7_MFbxKb26U5ug_AiVELXO_!
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
