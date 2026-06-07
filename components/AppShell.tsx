061c539210597bab5d88e33a2d1ef69fca866f9a
mariowakim38-blip <mariowakim38@gmail.com>
2026-06-07 18:17:26 +0300

    Delete components directory

--- components/AppShell.tsx
+++
@@ -1,2 +0,0 @@
-import Sidebar from './Sidebar'
-export default function AppShell({children}:{children:React.ReactNode}){return <div className="app-shell"><Sidebar/><main className="main">{children}</main></div>}
